9bcae4f42619b6dcf1337a3d5b8e29729825b832
payne-wu <wuzhipeng1289690157@gmail.com>
2022-06-24 15:18:05 +0800

    change yesask

--- YesAsk/Base.py
+++ YesAsk/Base.py
@@ -16,36 +16,40 @@ from YesAsk.configs import GET_TIMEOUT, MinSleepTime, MaxSleepTime
 requests.packages.urllib3.disable_warnings()
 
 
-# from fake_useragent import UserAgent
-# for _ in range(1, 2 << 12):
-#     print(UserAgent(verify_ssl=False).random)
-
-
 class BaseCrawler:
 
     @staticmethod
     @retry(stop_max_attempt_number=3, retry_on_result=lambda x: x is None, wait_fixed=2000)
     def fetch(method, url, **kwargs):
+        """
+        a base requests method
+        :param method:
+        :param url:
+        :param kwargs:
+        :return:
+        """
         try:
-            logger.info(f"scraping url: {url}")
+            logger.info(f"Scraping URL: {url}")
             if "proxies" in kwargs.keys():
-                logger.info(f"Use proxy {kwargs.values()}")
+                logger.debug(f"Use proxy {kwargs.values()}")
             kwargs.setdefault("timeout", GET_TIMEOUT)
             kwargs.setdefault("verify", False)
             kwargs.setdefault("headers", Headers(headers=True).generate())
             response = requests.request(method, url, **kwargs)
             if response.status_code == 200:
                 return response
-            logger.error(
-                f"Fetch status code is not as expected，status code: {response.status_code}"
-            )
-        except (requests.ConnectionError, requests.ReadTimeout, RetryError):
-            return
+            logger.error(f"Fetch status code is not as expected，status code: {response.status_code} URL: {url}")
+        except (requests.ConnectionError, requests.ReadTimeout, RetryError) as e:
+            logger.error(f"request exception : {e}")
         finally:
             time.sleep(random.uniform(MinSleepTime, MaxSleepTime))
 
     @staticmethod
     def cloud_proxy():
+        """
+        get dailiyun proxy
+        :return:
+        """
         url = "http://hlfjump.sh.nint.com/api/getproxyyun"
         params = {"user": "test", "proxycount": 1}
         resp_json = BaseCrawler.fetch(method="GET", url=url, params=params).json()
@@ -55,6 +59,7 @@ class BaseCrawler:
                 "http": f"http://liner0123:a123456@{proxy}",
                 "https": f"http://liner0123:a123456@{proxy}",
             }
+
 
 if __name__ == '__main__':
     print(BaseCrawler.cloud_proxy())
